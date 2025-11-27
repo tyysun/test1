688838533a575baa3f49e26b3b9e225533530d9f
“tyysun” <tyysun@outlook.com>
2025-11-27 19:53:30 +0800

    增加4个测试路由，增加跳转功能

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,57 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 
-import {RouterProvider ,createBrowserRouter } from 'react-router-dom'
-import { ErrorPage } from './components/ErrorPage'
+import { NavLink, Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RedirectToWelcome1 } from './components/RedirectToWelcome1'
 const router = createBrowserRouter([
-  { 
-    path: '/', 
-    element: <div >root</div>,
-    errorElement:<ErrorPage ></ErrorPage>},
   {
-    path: '/1',
-    element: <div>1</div>,
-  }
-]) 
+    path: '/',
+    element: <Outlet></Outlet>,
+    errorElement: <RedirectToWelcome1 ></RedirectToWelcome1>,
+    children: [
+      {
+        index: true,
+        element: <div >首页</div>
+      },
+      {
+        path: 'welcome',
+        element: <Outlet></Outlet>,
+        children: [
+          {
+            index: true,
+            element: <div >空</div>
+          },
+          {
+            path: '1',
+            element: (
+              <div >1,<NavLink to='/welcome/2'>下一页</NavLink></div>
+            )
+          },
+          {
+            path: '2',
+            element: (
+              <div >2,<NavLink to='/welcome/3'>下一页</NavLink></div>
+            )
+          },
+          {
+            path: '3',
+            element: (
+              <div >3,<NavLink to='/welcome/4'>下一页</NavLink></div>
+            )
+          },
+          {
+            path: '4',
+            element: (
+              <div >4,<NavLink to='/xxx'>开始</NavLink></div>
+            )
+          },
+        ]
+      }
+    ]
+
+  },
+
+])
 const div = document.getElementById('root') as HTMLElement
 const root = createRoot(div)
 root.render(
