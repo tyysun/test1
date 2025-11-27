4377d9369cff3eba8e91839643774fa24e072b4c
“tyysun” <tyysun@outlook.com>
2025-11-27 19:00:39 +0800

    引入react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import {App} from './App.tsx'
 
+import {RouterProvider ,createBrowserRouter } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  { path: '/', element: <div >root</div> },
+  {
+    path: '/1',
+    element: <div>1</div>,
+  }
+]) 
 const div = document.getElementById('root') as HTMLElement
 const root = createRoot(div)
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
